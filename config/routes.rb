4ef8cf794c7b105d3d72d346c2d5835cd09eebe2
Avinash Narayana Regulagadda <avinash.narayana@gmail.com>
2016-08-01 15:23:57 +0000

    Signup process

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'users/new'
-
+  
   get  '/about', to: 'static_pages#about'
-
+  
   get  '/contact', to: 'static_pages#contact'
   
   get  '/signup',  to: 'users#new'
-
-  root 'static_pages#home'
+  post '/signup',  to: 'users#create'
+  resources :users
 end
