5ac5816f1dd238b993e2b97865cf98f8e843086a
Avinash Narayana Regulagadda <avinash.narayana@gmail.com>
2016-08-01 19:01:04 +0000

    Made User RESTful

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render 'new'
@@ -16,5 +16,16 @@ class SessionsController < ApplicationController
   def destroy
     log_out
     redirect_to root_url
+  end
+  
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
