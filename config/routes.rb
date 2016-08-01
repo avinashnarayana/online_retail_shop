60510a2340e90c7677a3035c363c20a051080579
Avinash Narayana Regulagadda <avinash.narayana@gmail.com>
2016-08-01 12:19:16 +0000

    Added migrations and models for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get  '/about', to: 'static_pages#about'
 
   get  '/contact', to: 'static_pages#contact'
+  
+  get  '/signup',  to: 'users#new'
 
   root 'static_pages#home'
 end
