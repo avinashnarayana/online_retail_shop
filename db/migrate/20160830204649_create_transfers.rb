82344e39ee7b1b2afbda9f1cb79de29db0783ddf
Avinash Narayana Regulagadda <avinash.narayana@gmail.com>
2016-08-31 11:22:31 +0000

    Added references in migrations

--- db/migrate/20160830204649_create_transfers.rb
+++ db/migrate/20160830204649_create_transfers.rb
@@ -2,8 +2,8 @@ class CreateTransfers < ActiveRecord::Migration[5.0]
   def change
     create_table :transfers do |t|
       t.references :user, foreign_key: true
-      t.references :from_location, foreign_key: true
-      t.references :to_location, foreign_key: true
+      t.references :from_location, foreign_key: true, references: :locations
+      t.references :to_location, foreign_key: true, references: :locations
       t.references :product, foreign_key: true
       t.integer :quantity
       t.text :details
