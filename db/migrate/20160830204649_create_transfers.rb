88e2127b674dcfc16e97569f71cf0740b8884045
Avinash Narayana Regulagadda <avinash.narayana@gmail.com>
2016-08-31 11:30:34 +0000

    Added foreign key contraints in migrations

--- db/migrate/20160830204649_create_transfers.rb
+++ db/migrate/20160830204649_create_transfers.rb
@@ -2,13 +2,15 @@ class CreateTransfers < ActiveRecord::Migration[5.0]
   def change
     create_table :transfers do |t|
       t.references :user, foreign_key: true
-      t.references :from_location, foreign_key: true, references: :locations
-      t.references :to_location, foreign_key: true, references: :locations
+      t.references :from_location, references: :locations
+      t.references :to_location, references: :locations
       t.references :product, foreign_key: true
       t.integer :quantity
       t.text :details
 
       t.timestamps
+      add_foreign_key :transfers, :locations, column: :from_location
+      add_foreign_key :transfers, :locations, column: :to_location
     end
   end
 end
