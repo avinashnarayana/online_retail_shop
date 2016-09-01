8d1d0805b3060dcc5d17a38634047791c4937fde
Avinash Narayana Regulagadda <avinash.narayana@gmail.com>
2016-09-01 23:46:16 +0000

    Removed Product validation for stock

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,4 @@ class Product < ApplicationRecord
   default_scope -> { order(updated_at: :desc) }
   validates :name, presence: true
   validates :price, presence: true
-  validates :stock, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,20 @@
-User.create!(name:  "Avinash N Regulagadda",
-             email: "avinash.narayana@gmail.com",
+User.create!(name:  "Store Keeper",
+             email: "storekeeper@storekeeper.com",
              password:              "password",
              password_confirmation: "password",
              role: 1)   # 1 for store keeper/admin
              
-User.create!(name:  "Example User",
-             email: "example@example.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             role: 1)   # 1 for store keeper/admin
+User.create!(name:  "Store Assistant",
+             email: "storeassistant@storeassistant.com",
+             password:              "password",
+             password_confirmation: "password",
+             role: 2)   # 2 for store assistant
+
+User.create!(name:  "Customer",
+             email: "customer@customer.com",
+             password:              "password",
+             password_confirmation: "password",
+             role: 0)   # 0 for customer
 
 99.times do |n|
   name  = Faker::Name.name
@@ -19,3 +25,11 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
+
+Product.create(id: 1, name: "Amber Leaf",
+            description: "Tobacco 25g pack", 
+            price: 12.0, barcode: "5011054355409")
+Product.create(id: 2, name: "Germentin", 
+            description: "A medicine", price: 516.0, barcode: "509915101122")
+
+            
